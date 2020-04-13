ec8050b2856941fee2f78c21f11cc328884ae377
Matt Vella <mattvella07@gmail.com>
2020-04-13 14:28:34 -0500

    Added api gateway route /getUserInfo/{userId}

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -33,8 +33,36 @@ resource "aws_api_gateway_integration" "pelodata_login_integration" {
     uri                     = var.login_invoke_arn
 }
 
+resource "aws_api_gateway_resource" "pelodata_getUserInfo_resource" {
+    path_part   = "getUserInfo"
+    parent_id   = aws_api_gateway_rest_api.pelodata_apigateway.root_resource_id
+    rest_api_id = aws_api_gateway_rest_api.pelodata_apigateway.id
+}
+
+resource "aws_api_gateway_resource" "pelodata_getUserInfoFull_resource" {
+    path_part   = "{userId}"
+    parent_id   = aws_api_gateway_resource.pelodata_getUserInfo_resource.id
+    rest_api_id = aws_api_gateway_rest_api.pelodata_apigateway.id
+}
+
+resource "aws_api_gateway_method" "pelodata_getUserInfo_method" {
+    rest_api_id   = aws_api_gateway_rest_api.pelodata_apigateway.id
+    resource_id   = aws_api_gateway_resource.pelodata_getUserInfoFull_resource.id
+    http_method   = "GET"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "pelodata_getUserInfo_integration" {
+    rest_api_id             = aws_api_gateway_rest_api.pelodata_apigateway.id
+    type                    = "AWS_PROXY"
+    integration_http_method = "POST"
+    resource_id             = aws_api_gateway_resource.pelodata_getUserInfoFull_resource.id
+    http_method             = aws_api_gateway_method.pelodata_getUserInfo_method.http_method
+    uri                     = var.getUserInfo_invoke_arn
+}
+
 resource "aws_api_gateway_deployment" "pelodata_deployment" {
-    depends_on = [aws_api_gateway_integration.pelodata_login_integration]
+    depends_on = [aws_api_gateway_integration.pelodata_login_integration, aws_api_gateway_integration.pelodata_getUserInfo_integration]
 
     rest_api_id = aws_api_gateway_rest_api.pelodata_apigateway.id
     stage_name  = "Dev"
